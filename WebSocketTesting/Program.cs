02fc90b3ffa0d2543a0d82bbae73e1627412fbbe
紫菜 Seaweed <fengguisheng2008@163.com>
2024-09-07 18:43:03 +0800

    测试程序重写

--- WebSocketTesting/Program.cs
+++ WebSocketTesting/Program.cs
@@ -1,7 +1,17 @@
-﻿using System.Net.WebSockets;
+﻿using WebSocketSharp;
+using SeaBot.ApiModule;
 
 namespace WebSocketTesting
 {
+    enum EStatusCode
+    {
+        Success = 0,
+        Hello,
+        Processing,
+        Failed,
+        NotResponse = -1,
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -9,33 +19,27 @@ namespace WebSocketTesting
             Process();
         }
 
-        static async void Process()
+        static void Process()
         {
-            var socket = new ClientWebSocket();
-            socket.Options.KeepAliveInterval = TimeSpan.FromSeconds(500);
-            Console.WriteLine(socket.State);
-            Uri uri = new Uri("ws://127.0.0.1:8082/");
-            Console.WriteLine(uri.Host + ":" + uri.Port);
-            await socket.ConnectAsync(new Uri("ws://127.0.0.1:8082/"), CancellationToken.None);
-            await KeepConnectionActive(socket);
-            Console.WriteLine(socket.State);
-            Console.ReadKey();
+            using WebSocket ws = new("ws://127.0.0.1:8082/ws");
+            ws.OnOpen += (sender, e) => 
+            {
+                Console.WriteLine("Connected to the server.");
+            };
+            ws.OnClose += (sender, e) =>
+            {
+                Console.WriteLine("Connection closed");
+            };
+            ws.OnMessage += (sender, e) => 
+            {
+                Console.WriteLine("Received: " + e.Data);
+            };
+            ws.Connect();
             while (true)
             {
-                await socket.SendAsync(new byte[] { 1, 1, 1 }, WebSocketMessageType.Text, false, new CancellationToken());
-                Console.WriteLine("Sent");
-                Thread.Sleep(5000);
-                await socket.ReceiveAsync(new ArraySegment<byte>(), new CancellationToken());
-            }
-        }
-
-        static async Task KeepConnectionActive(ClientWebSocket clientWebSocket)
-        {
-            if (clientWebSocket.State == WebSocketState.Open)
-            {
-                // 这里可以发送和接收消息
-                Console.WriteLine("Press any key to exit...");
-                await Task.Run(() => Console.ReadKey());
+                ApiText.ApiTextHello hello = new();
+                hello.Action = "hello";
+                hello.StatusCode = (int)EStatusCode.NotResponse;
             }
         }
     }
